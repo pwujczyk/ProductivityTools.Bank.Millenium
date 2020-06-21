d8f0bd104f80495aea3a4281d53819b2855c600c
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-06-21 22:39:14 +0200

    initial commit

---
+++ ProductivityTools.Bank.Millenium.Runner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductivityTools.Bank.Millenium.Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
