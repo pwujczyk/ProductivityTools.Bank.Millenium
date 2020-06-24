7f6178ab750239779da8a1fe2e8087f322cb4e6f
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-06-24 13:32:04 +0200

    go to history

--- ProductivityTools.Bank.Millenium.Selenium/SeleniumCalls.cs
+++ ProductivityTools.Bank.Millenium.Selenium/SeleniumCalls.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Runtime.CompilerServices;
+using System.Threading;
 
 namespace ProductivityTools.Bank.Millenium.Selenium
 {
@@ -22,31 +23,45 @@ namespace ProductivityTools.Bank.Millenium.Selenium
 
             var loginButton = this.Chrome.FindElement(By.Id("BtnLogin"));
             loginButton.Click();
-
+            Thread.Sleep(2000);
             var passwordControl = this.Chrome.FindElement(By.Id("PasswordOne_txtContent"));
             passwordControl.SendKeys(password);
 
             bool[] peselInputs = new bool[11];
-            Func<string,bool> peselDisabled = (id) =>
+            Func<string,bool> peselEnabled = (id) =>
               {
-                  var pesel1Control = this.Chrome.FindElement(By.Id(id));
-                  var disabled = pesel1Control.GetAttribute("disabled");
+                  var peselControl = this.Chrome.FindElement(By.Id(id));
+                  var disabled = peselControl.GetAttribute("disabled");
                   return !(disabled == "true");
               };
            ;
-            peselInputs[0] = peselDisabled("PESEL_0_txtContent");
-            peselInputs[1] = peselDisabled("PESEL_1_txtContent");
-            peselInputs[2] = peselDisabled("PESEL_2_txtContent");
-            peselInputs[3] = peselDisabled("PESEL_3_txtContent");
-            peselInputs[4] = peselDisabled("PESEL_4_txtContent");
-            peselInputs[5] = peselDisabled("PESEL_5_txtContent");
-            peselInputs[6] = peselDisabled("PESEL_6_txtContent");
-            peselInputs[7] = peselDisabled("PESEL_7_txtContent");
-            peselInputs[8] = peselDisabled("PESEL_8_txtContent");
-            peselInputs[9] = peselDisabled("PESEL_9_txtContent");
-            peselInputs[10] = peselDisabled("PESEL_10_txtContent");
 
 
+            //peselInputs[0] = peselDisabled("PESEL_0_txtContent");
+            //peselInputs[1] = peselDisabled("PESEL_1_txtContent");
+            //peselInputs[2] = peselDisabled("PESEL_2_txtContent");
+            //peselInputs[3] = peselDisabled("PESEL_3_txtContent");
+            //peselInputs[4] = peselDisabled("PESEL_4_txtContent");
+            //peselInputs[5] = peselDisabled("PESEL_5_txtContent");
+            //peselInputs[6] = peselDisabled("PESEL_6_txtContent");
+            //peselInputs[7] = peselDisabled("PESEL_7_txtContent");
+            //peselInputs[8] = peselDisabled("PESEL_8_txtContent");
+            //peselInputs[9] = peselDisabled("PESEL_9_txtContent");
+            //peselInputs[10] = peselDisabled("PESEL_10_txtContent");
+
+            for (int i = 0; i < peselInputs.Length; i++)
+            {
+                string id = $"PESEL_{i}_txtContent";
+                if (peselEnabled(id))
+                {
+                    var peselControl = this.Chrome.FindElement(By.Id(id));
+                    peselControl.SendKeys(Pesel[i].ToString());
+                }
+            }
+
+            this.Chrome.FindElement(By.Id("BtnLogin")).Click();
+
+            this.Chrome.Url = "https://www.bankmillennium.pl/osobiste2/Accounts/CurrentAccountDetails/Details";
         }
     }
 }
